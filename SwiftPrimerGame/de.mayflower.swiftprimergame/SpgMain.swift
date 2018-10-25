3e7dc6dd85a4d7be6ff84d334733285feed0694c
Christopher Stock <email@christopherstock.de>
2018-10-25 14:49:22 +0200

    Added rect drawing.

--- SwiftPrimerGame/de.mayflower.swiftprimergame/SpgMain.swift
+++ SwiftPrimerGame/de.mayflower.swiftprimergame/SpgMain.swift
@@ -1,6 +1,16 @@
 import Foundation
 import UIKit
 
+/**
+ *  The main application class.
+ *
+ *  TODO Add main thread.
+ *  TODO Add UI controls via dragging.
+ *  TODO Add example game logic.
+ *  TODO Add documentation for all elements.
+ *  TODO Structure all classes to packages.
+ *  TODO Extract drawing methods to static methods in SpgDrawing.
+ */
 class SpgMain
 {
     public static func main()
--- SwiftPrimerGame/de.mayflower.swiftprimergame/SpgView.swift
+++ SwiftPrimerGame/de.mayflower.swiftprimergame/SpgView.swift
@@ -3,33 +3,46 @@ import Foundation
 
 class SpgView : UIView
 {
+/*
+    override init( frame : CGRect )
+    {
+        super.init( frame: frame )
+    }
+*/
+/*
+    required init( coder : NSCoder )
+    {
+        super.init( coder: coder )
+    }
+*/
     override func draw( _ rect : CGRect )
     {
         print( "SpgView.draw( rect ) being invoked" )
 
+        let WIDTH  :CGFloat = self.frame.size.width
+        let HEIGHT :CGFloat = self.frame.size.height
+
+        print( " View dimensions are [" + WIDTH.description + "]x[" + HEIGHT.description + "]" )
 
         self.drawExampleRectangle()
     }
 
     private func drawExampleRectangle()
     {
-        let center = CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
-        let rectangleWidth:CGFloat = 100.0
-        let rectangleHeight:CGFloat = 100.0
+        let rectangleWidth  :CGFloat = 100.0
+        let rectangleHeight :CGFloat = 200.0
 
         guard let ctx = UIGraphicsGetCurrentContext() else { return }
 
-        //4
-        ctx.addRect(CGRect(x: center.x - (0.5 * rectangleWidth), y: center.y - (0.5 * rectangleHeight), width: rectangleWidth, height: rectangleHeight))
-        ctx.setLineWidth(10)
-        ctx.setStrokeColor(UIColor.gray.cgColor)
+        ctx.beginPath()
+        ctx.setStrokeColor( UIColor.red.cgColor )
+        ctx.setLineWidth( 10 )
+        ctx.addRect( CGRect( x: 10, y: 20, width: rectangleWidth, height: rectangleHeight ) )
         ctx.strokePath()
 
-        //5
-        ctx.setFillColor(UIColor.green.cgColor)
-
-        ctx.addRect(CGRect(x: center.x - (0.5 * rectangleWidth), y: center.y - (0.5 * rectangleHeight), width: rectangleWidth, height: rectangleHeight))
-
+        ctx.beginPath()
+        ctx.setFillColor( UIColor.green.cgColor )
+        ctx.addRect( CGRect( x: 10, y: 20, width: rectangleWidth, height: rectangleHeight ) )
         ctx.fillPath()
     }
 }
