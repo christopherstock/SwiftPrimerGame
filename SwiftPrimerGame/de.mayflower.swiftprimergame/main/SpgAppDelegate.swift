de27f440301fdfe3da76cc74fa3d81105b6c1cc6
Christopher Stock <email@christopherstock.de>
2018-11-05 17:58:24 +0100

    New package for all game objects.

--- SwiftPrimerGame/de.mayflower.swiftprimergame/main/SpgAppDelegate.swift
+++ SwiftPrimerGame/de.mayflower.swiftprimergame/main/SpgAppDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 /**
  *  The main application delegate
  *
- *  TODO Create parent superclass for all game objects.
+ *  TODO Create parent superclass and new package for all game objects.
  *  TODO Add alert on reaching finish line
  *  TODO Add bg music
  *
