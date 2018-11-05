574a932ff9d55f61017fd733b3688d11b7412592
Christopher Stock <email@christopherstock.de>
2018-11-05 10:31:52 +0100

    Rearranged TODO list

--- SwiftPrimerGame/de.mayflower.swiftprimergame/main/SpgAppDelegate.swift
+++ SwiftPrimerGame/de.mayflower.swiftprimergame/main/SpgAppDelegate.swift
@@ -3,19 +3,14 @@ import UIKit
 /**
  *  The main application delegate
  *
- *  TODO Find smoother 'not' operator
- *
  *  TODO init() in viewController class .. remove statics
- *
  *  TODO Remove all ? everywhere
  *  TODO Remove ! in UIImage constructor
- *
  *  TODO Find better startup than SpgViewController
  *  TODO Redesign 'public' and 'private' everywhere
  *  TODO Offset for car to bottom
  *  TODO Add debug switch for debug bg blocks
  *  TODO Remove all ! .. everywhere
- *
  *  TODO Add finish line
  *  TODO Add obstacles
  *  TODO Add deco
@@ -25,9 +20,9 @@ import UIKit
  *  TODO Add bg music
  *  TODO Add game end on reaching bottom bound etc
  *
- *  TODO MINOR Fix simulator lag on touch input
  *  TODO MINOR Improve static access to SpgGame instance. Enable static access to ViewController or AppDelegate
  *  TODO MINOR Limit draw area to inner bounds. (without navbar and statusbar)
+ *  TODO MINOR Fix simulator lag on touch input
  *
  *  ASK  Should _ be used as parameter label
  *  ASK  How can UIImage be unboxed in constructor
